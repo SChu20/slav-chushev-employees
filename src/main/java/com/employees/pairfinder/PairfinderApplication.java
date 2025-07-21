4e9ea9c43ac252a8990fca4f5c6c5548aaf26146
Slav Chushev <chushev.slav@gmail.com>
2025-07-21 15:46:34 +0300

    Initial commit

---
+++ src/main/java/com/employees/pairfinder/PairfinderApplication.java
@@ -0,0 +1,13 @@
+package com.employees.pairfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PairfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PairfinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/employees/pairfinder/PairfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employees.pairfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PairfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
